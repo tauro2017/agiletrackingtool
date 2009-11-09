f055b9f26bb42e5b02c147a14bb65ab58b57a4fc
ben.schreur <ben.schreur@72e2e9c2-bc0c-11de-b528-055f0b42e881>
2009-11-09 17:49:17 +0000

    Update install_and_run script to only install grails when not present.

--- installscripts/install_and_run.sh
+++ installscripts/install_and_run.sh
@@ -10,25 +10,37 @@ export TRACKING_TOOL_CODE_DIR="$INSTALL_DIR/agiletrackingtool-read-only"
 
 export GRAILS_ZIP="grails-bin-1.1.1.zip"
 export GRAILS_ZIP_LOCATION="http://dist.codehaus.org/grails"/
+export GRAILS_PLUGIN_URL="http://plugins.grails.org/grails-google-chart/tags/RELEASE_0_5_1/grails-google-chart-0.5.0.zip"
 
 #-------------------------------------------------------------------------------
 function test_if_java_environment_is_present {
 	if [[ -de $JAVA_HOME ]]; then
-		echo "Test for JAVA environment succesfull."
+		echo "Test for Java environment succesfull."
 	else
 		echo "No JAVA_HOME detected. Make sure a JAVA SDK is installed."
 		exit
 	fi
 }
 
-function test_if_executable_is_present {
-	which $1 > /dev/null
-	if [ $? = 0 ]; then
-		echo "Executable $1 found!"
+function install_grails_environment_if_needed {
+	if [[ -de $GRAILS_HOME ]]; then
+		echo "Test for Grails environment succesfull."
 	else
-		echo "Executable $1 not found. Please install. For instance by using: sudo apt-get install $1"
-		exit
+		echo "Retrieving the Grails environment:"
+		wget $GRAILS_ZIP_LOCATION/$GRAILS_ZIP
+		unzip $GRAILS_ZIP exit
+		export GRAILS_HOME="$INSTALL_DIR/grails-1.1.1"
 	fi
+}
+
+function test_if_executable_is_present {
+        which $1 > /dev/null
+        if [ $? = 0 ]; then
+                echo "Executable $1 found!"
+        else
+                echo "Executable $1 not found. Please install. For instance by using: sudo apt-get install $1"
+                exit
+        fi
 }
 #-------------------------------------------------------------------------------
 
@@ -40,16 +52,13 @@ test_if_executable_is_present svn
 mkdir $INSTALL_DIR
 cd $INSTALL_DIR
 
-echo "Retrieving the Grails environment:"
-wget $GRAILS_ZIP_LOCATION/$GRAILS_ZIP
-unzip $GRAILS_ZIP 
+install_grails_environment_if_needed
 
 echo "Retrieving the Agile Tracking Tool source code to $TRACKING_TOOL_DIR:"
 svn checkout http://agiletrackingtool.googlecode.com/svn/trunk/ $TRACKING_TOOL_CODE_DIR
 
 cd $TRACKING_TOOL_CODE_DIR
-export GRAILS_HOME="$INSTALL_DIR/grails-1.1.1"
-grails install-plugin http://plugins.grails.org/grails-google-chart/tags/RELEASE_0_5_1/grails-google-chart-0.5.0.zip
-grails run-app
 
+grails install-plugin $GRAILS_PLUGIN_URL
+grails run-app 
 
