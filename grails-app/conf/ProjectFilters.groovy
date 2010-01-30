5dc0ae4da220a73793663f309e5d9aaf61eda76b
ben.schreur <ben.schreur@72e2e9c2-bc0c-11de-b528-055f0b42e881>
2010-01-30 14:35:19 +0000

    Adding license headers to new files

--- grails-app/conf/ProjectFilters.groovy
+++ grails-app/conf/ProjectFilters.groovy
@@ -1,3 +1,24 @@
+/*----------------------------------------------------------------------------
+Project: Agile Tracking Tool
+
+Copyright 2008, 2009, 2010   Ben Schreur
+------------------------------------------------------------------------------
+This file is part of Agile Tracking Tool.
+
+Agile Tracking Tool is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Agile Tracking Tool is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Agile Tracking Tool.  If not, see <http://www.gnu.org/licenses/>.
+------------------------------------------------------------------------------*/
+
 class ProjectFilters {
 	def cookieService
 	def projectIdCookieString = "projectId"
