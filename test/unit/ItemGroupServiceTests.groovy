c62ae05bc7c6b124fe022c76121f4b33f618f9fc
ben.schreur <ben.schreur@72e2e9c2-bc0c-11de-b528-055f0b42e881>
2010-02-14 16:28:15 +0000

    - Added testcases for ItemGroup(+Service)

--- test/unit/ItemGroupServiceTests.groovy
+++ test/unit/ItemGroupServiceTests.groovy
@@ -1,37 +1,70 @@
 import grails.test.*
 
 class ItemGroupServiceTests extends GrailsUnitTestCase {
+	def itemGroupService
+
+	def project
+	
+	def groupA
+	def groupB
+	def groups
+	
+	def itemsA
+    def itemsB
+    def items
+
     protected void setUp() {
         super.setUp()
+        
+        itemGroupService = new ItemGroupService()
+        
+        project = Defaults.getProjects(1)[0]
+    	mockDomain(Project, [project] )
+    	
+        groups = Defaults.getGroups(2,[project])
+        (groupA, groupB) = groups
+		mockDomain(ItemGroup, groups)
+		
+		itemsA = Defaults.getItems(2,[groupA],project,1)
+     	itemsB = Defaults.getItems(3,[groupB],project,10)
+     	items = (itemsA + itemsB)
+     	mockDomain(Item, items)
     }
 
     protected void tearDown() {
         super.tearDown()
     }
 
-     void testDeleteWholeGroup()
+    void testDeleteWholeGroup()
     {
-    	def project = Defaults.getProjects(1)[0]
-    	mockDomain(Project, [project] )
-    	
-		def groups = Defaults.getGroups(2,[project])
-		mockDomain(ItemGroup, groups)
-    	def (groupA, groupB) = groups
-    	
-    	def itemsA = Defaults.getItems(2,[groupA],project,1)
-    	def itemsB = Defaults.getItems(3,[groupB],project,10)
-    	def items = (itemsA + itemsB)
-    	mockDomain(Item, items)
-    	
     	def iteration = Defaults.getIterations(1,project)[0]
     	mockDomain(Iteration, [iteration] )
     	
     	items.each{ iteration.addItem(it) }
     	
-    	new ItemGroupService().deleteWholeGroup(groupA)
+    	itemGroupService.deleteWholeGroup(groupA)
     	
     	assertEquals 1, ItemGroup.count()
     	assertEquals itemsB.size(), Item.count()
     	assertEquals itemsB.size(), iteration.items.size()
     }
+    
+    void testTransformToItemsByGroup()
+    {
+		def itemsByGroup = itemGroupService.transformToItemsByGroup(groups,items)
+		
+		assertEquals groups.size(), itemsByGroup.size()
+		assertEquals itemsA, itemsByGroup[groupA] 
+		assertEquals itemsB, itemsByGroup[groupB]
+    }
+    
+    void testTransformToItemsByGroupWhenNoItemsPresent()
+    {
+    	def itemsByGroup = itemGroupService.transformToItemsByGroup(groups,[])
+		
+		assertEquals groups.size(), itemsByGroup.size()
+		assertEquals 0, itemsByGroup[groupA].size() 
+		assertEquals 0, itemsByGroup[groupB].size()
+    }
+    
 }
