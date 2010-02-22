ee62e6dca0410de884ce78736fd79b450336a0e6
ben.schreur <ben.schreur@72e2e9c2-bc0c-11de-b528-055f0b42e881>
2010-02-22 22:33:38 +0000

    Added test for ProjectService

--- trunk/test/unit/ProjectServiceTests.groovy
+++ trunk/test/unit/ProjectServiceTests.groovy
@@ -36,36 +36,62 @@ class ProjectServiceTests extends GrailsUnitTestCase {
     
     void testExportProjectToXmlString()
     {
-    	def project = Defaults.getProjects(1)[0]
-    	mockDomain(Project, [project] )
-    	
-		def groups = Defaults.getGroups(Util.random(3..8), project)
-		mockDomain(ItemGroup, groups )
-		
-		def items = Defaults.getItems(Util.random(5..10), groups, project, 10 )
-		mockDomain(Item, items )
-		
-		def iterations = Defaults.getIterations(Util.random(2..5), project)
-		mockDomain(Iteration, iterations )
-		
-		def now = new Date()
-		def snapShots = Defaults.getSnapShots(groups, now-10, now-5, project)
-		mockDomain(PointsSnapShot, snapShots )
-    
-    	def control = mockFor(UtilXml)
-    	def anyString = "Now is here"
-    	control.demand.static.exportToXmlString(1..1) { 
-    	    _project, _groups, _items, _iterations, _pointsSnapShots, _exportDate, _docVersion ->
-    	    	assertEquals project, _project
-    	    	assertEquals items.size(), _items.size()
-    	    	assertEquals groups.size(), _groups.size()
-    	    	assertEquals iterations.size(), _iterations.size()
-    	    	assertEquals snapShots.size(), _pointsSnapShots.size()
-    	   		return anyString 
-    	}
-    	
-    	assertEquals anyString, projectService.exportToXmlString(project)
-    	control.verify()
+        def projectControl = mockFor(Project)
+        def groupControl = mockFor(ItemGroup)
+        def itemControl = mockFor(Item)
+        def iterationControl = mockFor(Iteration)
+        def snapshotControl = mockFor(PointsSnapShot)
+        
+        def project,groups,items,iterations,snapshots
+        (project,groups,items,iterations,snapshots) = (1..5).collect{ new Object() }
+        groupControl.demand.static.findAllByProject(1..1) { _project -> return groups }
+        itemControl.demand.static.findAllByProject(1..1) { _project -> return items }
+        iterationControl.demand.static.findAllByProject(1..1) { _project -> return iterations }
+        snapshotControl.demand.static.findAllByProject(1..1) { _project -> return snapshots }
+        
+        def utilXmlControl = mockFor(UtilXml)
+        def anyString = "Now is here"
+        utilXmlControl.demand.static.exportToXmlString(1..1) { 
+            _project, _groups, _items, _iterations, _pointsSnapShots, _exportDate, _docVersion ->
+                assertSame project, _project
+                assertSame items, _items
+                assertSame groups, _groups
+                assertSame iterations, _iterations
+                assertSame snapshots, _pointsSnapShots
+                   return anyString
+        }
+        
+        assertEquals anyString, projectService.exportToXmlString(project)
+        
+        def mockControllers = [projectControl,groupControl,itemControl,iterationControl,snapshotControl, utilXmlControl]
+        mockControllers*.verify()
     }
-   
+    
+    void testDeleteWholeProject()
+    {
+        def project = Defaults.getProjects(1)[0]
+        mockDomain(Project, [project])
+        
+        def now = new Date()
+        mockDomain(PointsSnapShot, Defaults.getSnapShots([], now-10, now, project) )
+        
+        def group = new Object()
+        def groupControl = mockFor(ItemGroup)
+        groupControl.demand.static.findAllByProject(1..1) { _project -> return [ group ] }        
+        def itemGroupServiceControl = mockFor(ItemGroupService)
+        itemGroupServiceControl.demand.static.deleteWholeGroup(1..1) { _group -> assertSame group, _group}
+        
+        def iteration = new Object()
+        def iterationControl = mockFor(Iteration)
+        iterationControl.demand.static.findAllByProject(1..1) { _iteration-> return [iteration] }
+        def iterationServiceControl = mockFor(IterationService)
+        iterationServiceControl.demand.static.unloadItemsAndDelete(1..1) { _iteration -> assertSame iteration, _iteration }
+        
+        projectService.iterationService = iterationServiceControl.createMock()
+        projectService.itemGroupService = itemGroupServiceControl.createMock()
+        projectService.delete(project)
+        
+        [iterationServiceControl, iterationServiceControl]*.verify()
+        assertEquals 0, Project.count()
+    }
 }
