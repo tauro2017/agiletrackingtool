c27a75f3367ccd7eb784081f3ca7b2409a741b83
ben.schreur <ben.schreur@72e2e9c2-bc0c-11de-b528-055f0b42e881>
2010-03-01 23:03:10 +0000

    Invalidate session on logout

--- branches/users/grails-app/controllers/LogoutController.groovy
+++ branches/users/grails-app/controllers/LogoutController.groovy
@@ -4,7 +4,7 @@ class LogoutController {
 	 * Index action. Redirects to the Spring security logout uri.
 	 */
 	def index = {
-		// TODO  put any pre-logout code here
-		redirect(uri: '/j_spring_security_logout')
+		session.invalidate()
+		redirect(controller:'login')
 	}
 }
