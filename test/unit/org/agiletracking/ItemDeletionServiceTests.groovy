5aa51b7e98f13985d4240428789f2cffc15888f0
ben.schreur <ben.schreur@72e2e9c2-bc0c-11de-b528-055f0b42e881>
2011-01-23 10:38:08 +0000

    Commit ItemDeletionService

---
+++ test/unit/org/agiletracking/ItemDeletionServiceTests.groovy
@@ -0,0 +1,17 @@
+package org.agiletracking
+
+import grails.test.*
+
+class ItemDeletionServiceTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
