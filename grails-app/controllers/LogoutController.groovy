c612344e2f60dbf7a35a20b672000cf53e625a2e
praskrishna@gmail.com <praskrishna@gmail.com@72e2e9c2-bc0c-11de-b528-055f0b42e881>
2012-04-22 02:24:22 +0000

    Implemented authorization. With Admin and Project Manager. There is still some issue, need to be fixed.

--- grails-app/controllers/LogoutController.groovy
+++ grails-app/controllers/LogoutController.groovy
@@ -4,7 +4,7 @@ class LogoutController {
 	 * Index action. Redirects to the Spring security logout uri.
 	 */
 	def index = {
-	     session.invalidate()
-             redirect(uri: '/j_spring_security_logout')
+		session.invalidate()
+		redirect(uri: '/login/auth')
 	}
 }
